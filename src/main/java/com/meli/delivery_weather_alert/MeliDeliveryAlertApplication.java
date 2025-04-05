86824f79f575780332de0b9370db45dc8f17ebeb
Danilo Ramirez <danilo0613@hotmail.com>
2025-04-04 21:54:26 -0500

    first commit

---
+++ src/main/java/com/meli/delivery_weather_alert/MeliDeliveryAlertApplication.java
@@ -0,0 +1,13 @@
+package com.meli.delivery_weather_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeliDeliveryAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeliDeliveryAlertApplication.class, args);
+	}
+
+}
